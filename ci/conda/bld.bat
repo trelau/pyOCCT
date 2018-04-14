518666723a1bb5389074a7bb654335f77c8ad45a
Trevor Laughlin <trevor@laughlinresearch.com>
2018-04-14 16:54:14 -0400

    Install via conda build and app badges in readme

--- ci/conda/bld.bat
+++ ci/conda/bld.bat
@@ -3,8 +3,6 @@ cd build
 
 cmake .. -G "Ninja" ^
     -DCMAKE_BUILD_TYPE="Release" ^
-    -DCMAKE_PREFIX_PATH:FILEPATH="%PREFIX%" ^
-    -DCMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
     -DPTHREAD_INCLUDE_DIRS:FILEPATH="%LIBRARY_PREFIX%/include" ^
     -DENABLE_SMESH=ON ^
     -DENABLE_NETGEN=ON ^
@@ -15,3 +13,6 @@ cmake .. -G "Ninja" ^
 if errorlevel 1 exit 1
 ninja install
 if errorlevel 1 exit 1
+
+cd ..
+%PYTHON% setup.py install
