44ae1d7ba075e958afbf40f36d34315db6782d90
Trevor Laughlin <trevor@laughlinresearch.com>
2018-05-22 19:29:06 -0400

    Update build.sh -Use latest smesh and netgen on conda-forge -Add run_test.sh

--- ci/conda/build.sh
+++ ci/conda/build.sh
@@ -1,11 +1,12 @@
 mkdir build
 cd build
 
-export CC=gcc-4.9
-export CXX=g++-4.9
+# export CC=gcc-4.9
+# export CXX=g++-4.9
 
 cmake .. -G "Ninja" \
     -DCMAKE_BUILD_TYPE="Release" \
+    -DPTHREAD_INCLUDE_DIRS=${PREFIX} \
     -DENABLE_SMESH=ON \
     -DENABLE_NETGEN=ON \
     -DENABLE_BLSURF=OFF \
@@ -13,9 +14,7 @@ cmake .. -G "Ninja" \
     -DENABLE_WARNINGS=OFF \
     -DTEST_BUILD=OFF
 
-export NINJA_STATUS="[%f/%t(%r)] "
-
-ninja -j2 install
+ninja install
 
 cd ..
-$PYTHON setup.py install
+${PYTHON} setup.py install
