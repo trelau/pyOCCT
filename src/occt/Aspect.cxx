6dbd600d079ef28cade227ac541253d46ee6c82e
Trevor Laughlin <trevor@laughlinresearch.com>
2020-01-12 12:07:06 -0500

    Include xTypes.h in Aspect module for non-Windows

--- src/occt/Aspect.cxx
+++ src/occt/Aspect.cxx
@@ -20,6 +20,11 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 #include <pyOCCT_Common.hxx>
+
+#ifndef _WIN32
+#include <xTypes.h>
+#endif
+
 #include <Aspect_TypeOfLine.hxx>
 #include <Aspect_TypeOfMarker.hxx>
 #include <Aspect_TypeOfDeflection.hxx>
