45d89fd49a4fc9746e8c735622556913147b58f1
Trevor Laughlin <trevor@laughlinresearch.com>
2020-02-17 09:45:03 -0500

    Only build Standard module for azure testing...

--- ci/conda/bld.bat
+++ ci/conda/bld.bat
@@ -3,8 +3,8 @@ cd build
 
 cmake .. -G "Ninja" ^
     -DCMAKE_BUILD_TYPE="Release" ^
-    -DENABLE_SMESH=ON ^
-    -DENABLE_NETGEN=ON ^
+    -DENABLE_SMESH=OFF ^
+    -DENABLE_NETGEN=OFF ^
     -DENABLE_FORCE=OFF ^
     -DPTHREAD_INCLUDE_DIRS:FILEPATH="%LIBRARY_PREFIX%/include"
 if errorlevel 1 exit 1
