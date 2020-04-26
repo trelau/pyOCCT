9136bdd3018bc1af7ff7652296905ad014b277f2
frmdstryr <frmdstryr@protonmail.com>
2020-04-26 15:13:58 -0400

    Add j2 to for windows

--- ci/conda/bld.bat
+++ ci/conda/bld.bat
@@ -12,10 +12,10 @@ cmake .. -G "Ninja" ^
     -DPTHREAD_INCLUDE_DIRS:FILEPATH="%LIBRARY_PREFIX%/include"
 if errorlevel 1 exit 1
 
-ninja
+ninja -j2
 if errorlevel 1 exit 1
 
-ninja install
+ninja install -j2
 if errorlevel 1 exit 1
 
 cd ..
