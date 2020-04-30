fcd98a93a032dace037d653825c98f45bdcb249b
Trevor Laughlin <trevor@laughlinresearch.com>
2020-04-29 21:43:31 -0400

    Just build Standard for debugging... - Combine meta.yaml to just build section

--- ci/conda/bld.bat
+++ ci/conda/bld.bat
@@ -6,16 +6,16 @@ cd build
 
 cmake .. -G "Ninja" ^
     -DCMAKE_BUILD_TYPE="Release" ^
-    -DENABLE_SMESH=ON ^
-    -DENABLE_NETGEN=ON ^
+    -DENABLE_SMESH=OFF ^
+    -DENABLE_NETGEN=OFF ^
     -DENABLE_FORCE=OFF ^
     -DPTHREAD_INCLUDE_DIRS:FILEPATH="%LIBRARY_PREFIX%/include"
 if errorlevel 1 exit 1
 
-ninja -j2
+ninja
 if errorlevel 1 exit 1
 
-ninja install -j2
+ninja install
 if errorlevel 1 exit 1
 
 cd ..
