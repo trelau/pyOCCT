6958b562bd368a64479b04b9963fff6297eeef20
Trevor Laughlin <trevor@laughlinresearch.com>
2020-05-25 17:58:56 -0400

    Update setup.py after removing tools

--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@ from setuptools import setup
 setup(
     name='OCCT',
     version='7.4.0.0',
-    packages=['OCCT', 'OCCT.Boolean', 'OCCT.Exchange', 'OCCT.Topology',
-              'OCCT.Visualization'],
-    package_data={'OCCT': ['*.so', '*.pyd', '*.dll',
-                           'Visualization/_resources/*']},
+    packages=['OCCT', 'OCCT.Exchange', 'OCCT.Visualization'],
+    package_data={'OCCT': ['*.so', '*.pyd', '*.dll', 'Visualization/_resources/*']},
     author='Trevor Laughlin',
     description='Python bindings for OpenCASCADE.',
     url='https://github.com/LaughlinResearch/pyOCCT',
