33662f84ceca766af540cdd8d7bd237f6affd381
Trevor Laughlin <trevor.w.laughlin@gmail.com>
2021-04-04 12:02:27 -0400

    Remove cdt, add cross-linux.cmake

--- ci/conda/build.sh
+++ ci/conda/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ ${HOST} =~ .*linux.* ]]; then
+    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+fi
+
 # Fail on error
 set -e
 
@@ -8,6 +13,7 @@ mkdir build
 cd build
 
 cmake .. -G "Ninja" \
+    ${CMAKE_PLATFORM_FLAGS[@]} \
     -DCMAKE_BUILD_TYPE="Release"
 
 ninja -j1 install
