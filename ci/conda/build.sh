1cbb62fe736c913c7d19288569f6aeb7dc65743c
Trevor Laughlin <trevor.w.laughlin@gmail.com>
2021-04-04 10:59:00 -0400

    Remove cross-linux.cmake command

--- ci/conda/build.sh
+++ ci/conda/build.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-declare -a CMAKE_PLATFORM_FLAGS
-if [[ ${HOST} =~ .*linux.* ]]; then
-    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
-fi
-
 # Fail on error
 set -e
 
@@ -13,7 +8,6 @@ mkdir build
 cd build
 
 cmake .. -G "Ninja" \
-    ${CMAKE_PLATFORM_FLAGS[@]} \
     -DCMAKE_BUILD_TYPE="Release"
 
 ninja -j1 install
