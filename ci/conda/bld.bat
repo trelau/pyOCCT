fbef258d941b1b1cf2ff9bf3c4c55ada67ac5b05
Trevor Laughlin <trevor.w.laughlin@gmail.com>
2021-04-26 20:59:17 -0400

    Python3 find strategy

--- ci/conda/bld.bat
+++ ci/conda/bld.bat
@@ -6,7 +6,7 @@ cmake -G "Ninja" ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_SYSTEM_PREFIX_PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE="Release" ^
-  -DPython_FIND_VIRTUALENV=ON ^
+  -DPython_FIND_VIRTUALENV=ONLY ^
   ..
 
 if errorlevel 1 exit 1
