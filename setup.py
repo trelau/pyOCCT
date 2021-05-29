e366e2606a8b6e0917c6bd099592635daad9540a
Trevor Laughlin <trevor.w.laughlin@gmail.com>
2021-05-29 10:10:25 -0400

    Update to OCCT 7.5.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='OCCT',
-    version='7.5.1.2',
+    version='7.5.2.0',
     packages=['OCCT', 'OCCT.Exchange', 'OCCT.Visualization'],
     package_data={'OCCT': ['*.so', '*.pyd', '*.dll', 'Visualization/_resources/*']},
     author='Trevor Laughlin',
